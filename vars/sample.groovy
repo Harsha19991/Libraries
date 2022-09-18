87f4878269995e6a69c4b4275616539515396c15
Harsha19991 <112700318+Harsha19991@users.noreply.github.com>
2022-09-18 19:14:55 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -6,3 +6,7 @@ def Maven()
 {
   sh 'mvn package'
 }
+def Deploy()
+{
+  deploy adapters: [tomcat9(credentialsId: 'a78809e7-5c9e-44bd-9b0c-94964d56e9c7', path: '', url: 'http://172.31.43.181:8080')], contextPath: 'SIT', war: '**/*.war'
+}
