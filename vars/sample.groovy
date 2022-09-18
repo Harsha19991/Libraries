69e40c4c10c3fbacef42e4799667ccd5337fc30b
Harsha19991 <112700318+Harsha19991@users.noreply.github.com>
2022-09-18 19:19:35 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -10,3 +10,7 @@ def Deploy()
 {
   deploy adapters: [tomcat9(credentialsId: 'a78809e7-5c9e-44bd-9b0c-94964d56e9c7', path: '', url: 'http://172.31.43.181:8080')], contextPath: 'SIT', war: '**/*.war'
 }
+def Testing()
+{
+ sh 'java -jar /var/lib/jenkins/workspace/DeclerativePipeline/testing.jar'
+}
